82270cccad0f0328d22244e750f7a86f7648e988
Alfredo <alfredo.solano@the-cocktail.com>
2010-05-08 00:41:55 +0200

    estilos para pop ups

--- app/helpers/incident_helper.rb
+++ app/helpers/incident_helper.rb
@@ -3,7 +3,7 @@ module IncidentHelper
     js_code = ""
     div_info = ""
     incidents.each do |incident|
-     div_info ="'<div class=\"map_pop\"><p class=\"line_number line_#{incident.line.number}\">#{incident.line.number}</p><p class=\"station\">#{incident.station.name}</p><p>Incidencia: #{incident.comment}</p></div>'"
+     div_info ="'<div class=\"map_pop\"><p><span class=\"line_number line_#{incident.line.number}\">#{incident.line.number}</span><span class=\"station\">#{incident.station.name}</span></p><p class=\"date\">#{l(incident.date, :format => "long")}</p><p class=\"comment\">Incidencia: #{incident.comment}</p></div>'"
      js_code << "map.addOverlay(addInfoWindowToMarker(new GMarker("
      js_code << "new GLatLng(#{incident.lat},#{incident.long}),"
      js_code << "{title : 'Incidencia en línea #{incident.line_id}'}),"
