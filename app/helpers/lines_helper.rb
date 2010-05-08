21da230d4444bbea390e0f0da2caad0b15ce9bb6
José Luis Fernández <joseluis.fernandez@the-cocktail.com>
2010-05-08 13:05:39 +0200

    primera version de mapa por linea con la linea pintada a mano

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,2 +1,12 @@
 module LinesHelper
+  def create_map_line(line)
+    js ="var polyline = new GPolyline(["
+    line.stations.each do |station|
+      js << "new GLatLng(#{station.lat}, #{station.long}),"
+    end  
+    js << "],'#FF0000', 5);"
+    js << "map.addOverlay(polyline);"
+    
+    return js
+  end
 end
