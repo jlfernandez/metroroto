388c8822a72bb6d34fea9c9023aa4822f87851c8
José Luis Fernández <joseluis.fernandez@the-cocktail.com>
2010-05-09 18:28:41 +0200

    poniendo bien la ruta del icono

--- app/helpers/incident_helper.rb
+++ app/helpers/incident_helper.rb
@@ -5,7 +5,7 @@ module IncidentHelper
     js_code << "var metroIcon = new GIcon(G_DEFAULT_ICON);"
     incidents.each do |incident|
      div_info ="'<div class=\"map_pop\"><p><span class=\"line_number line_#{incident.line.number}\">#{incident.line.number}</span><span class=\"station\">#{incident.station.name}</span></p><p class=\"date\">#{l(incident.date, :format => "long")}</p><p class=\"comment\">Incidencia: #{incident.comment}</p></div>'"
-     js_code << "metroIcon.image = 'images/#{incident_icon incident}';"  
+     js_code << "metroIcon.image = '/images/#{incident_icon incident}';"  
      js_code << "map.addOverlay(addInfoWindowToMarker(new GMarker("
      js_code << "new GLatLng(#{incident.lat},#{incident.long}),"
      js_code << "{title : 'Incidencia en línea #{incident.line_id}', icon:metroIcon}),"
