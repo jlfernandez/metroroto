2626ffa8ab44be5a842ca41d0b3cb606eed14816
Alfredo <alfredo@cantorrodista.com>
2010-05-19 00:32:23 +0200

    cambio groso de polylinea era muy exagerado

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -4,7 +4,7 @@ module LinesHelper
     line.stations.each do |station|
       js << "new GLatLng(#{station.lat}, #{station.long}),"
     end  
-    js << "],'#{line.colour}', 8,1);"
+    js << "],'#{line.colour}', 4,1);"
     js << "map.addOverlay(polyline);"
     
     return js
