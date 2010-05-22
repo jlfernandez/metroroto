cdc8bbb535c9e4a53f8b255477597154723333db
vieron <javiersanchezmarin@gmail.com>
2010-05-22 18:49:12 +0200

    reuso el partial de últimas incidencias en el show de lineas

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,6 +2,7 @@ class LinesController < ApplicationController
   
   def show 
     @line = Line.find(params[:id])
+    @incidents = @line.incidents
     respond_to do |wants|
       wants.html
       wants.atom { render :xml => collection_to_feed(@line.incidents).to_xml }
