2c8f6fa6d23f9dc9910d3b000eea4fe10a4f6431
José Luis Fernández <joseluis.fernandez@the-cocktail.com>
2010-05-24 15:31:49 +0200

    quitando migracion FAIL

--- db/migrate/20100523172501_add_twitt_text_to_failed_twitts.rb
+++
@@ -1,9 +0,0 @@
-class AddTwittTextToFailedTwitts < ActiveRecord::Migration
-  def self.up
-    add_column :failed_twitts, :twitt_body, :string
-  end
-
-  def self.down
-    remove_column :failed_twitts, :twitt_body
-  end
-end
