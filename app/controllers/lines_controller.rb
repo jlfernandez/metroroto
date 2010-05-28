5bf360332ca954ee0d3c0d174ea5ef0ce892d5b0
José Luis Fernández <joseluis.fernandez@the-cocktail.com>
2010-05-28 10:08:03 +0200

    poniendo el objeto, no el id

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,7 +2,7 @@ class LinesController < ApplicationController
   
   def show 
     @line = Line.find(params[:id])
-    @incidents = Incident.last_incidents.by_line @line.id
+    @incidents = Incident.last_incidents.by_line @line
     respond_to do |wants|
       wants.html
       wants.atom { render :xml => collection_to_feed(@incidents).to_xml }
