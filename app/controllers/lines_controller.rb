35e816ed51a7cfc334af54bea4722e6abb659883
José Luis Fernández <joseluis.fernandez@the-cocktail.com>
2010-05-28 10:00:37 +0200

    poniendo en las lineas solo las incidencias de hoy

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,10 +2,10 @@ class LinesController < ApplicationController
   
   def show 
     @line = Line.find(params[:id])
-    @incidents = @line.incidents
+    @incidents = Incident.last_incidents.by_line @line.id
     respond_to do |wants|
       wants.html
-      wants.atom { render :xml => collection_to_feed(@line.incidents).to_xml }
+      wants.atom { render :xml => collection_to_feed(@incidents).to_xml }
     end  
   end  
   
