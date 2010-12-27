4f83a55fc30282bf11538aa1f04f4e0723412b96
Juan Lupion <juan.lupion@the-cocktail.com>
2010-12-27 15:03:33 +0100

    Un par de initializers más

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metroroto::Application.config.secret_token = '7f27343c4d7feb17d35f6ee7b8847bcdbe3a74e086d24b367175cebf0ce5075cd82f09b9f17584d71d3fb7c59112c7ec7383b5c34ebc85fdf0d05ddaccbde8b3'
