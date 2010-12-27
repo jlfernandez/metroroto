086d8ff3bfea4d0534cec92e4a2b291a223b4464
Juan Lupion <juan.lupion@the-cocktail.com>
2010-12-27 15:02:30 +0100

    Esto no sé muy bien lo que es, pero la instlación de Rails lo ha cambiado

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_metroroto_session',
-  :secret      => 'dedb3794c244a79f1028fcafd80dee1c145a825d5f0e95986be85419709e80fe4e34bf2c80da405f22e644568284d10976e3671fba3aab62752752344845668c'
-}
+Metroroto::Application.config.session_store :cookie_store, :key => '_metroroto_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Metroroto::Application.config.session_store :active_record_store
